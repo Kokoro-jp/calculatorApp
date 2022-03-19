f61cf0fdbd4641967c5097bbf5e9259cb05e9bc0
EC2 Default User <ec2-user@ip-172-31-46-96.ap-northeast-3.compute.internal>
2022-03-19 04:30:27 +0000

    提出1回目

---
+++ main.js
@@ -0,0 +1,36 @@
+let result = document.getElementById("display");
+
+function equal() {
+    result.innerHTML = eval(result.innerHTML);
+}
+
+function reset() {
+    result.innerHTML ="";
+}
+
+function clickbutton(target){
+    let input = target.innerHTML;
+    if(result.innerHTML == "0" || result.innerHTML == "00") {
+        result.innerHTML = input;
+    }else{
+        result.innerHTML +=input;
+    }
+}
+
+function edit(target){
+    let input = target.innerHTML;
+    
+    if(result.innerHTML.slice(-1) == "-") {
+        return;
+    }else if(result.innerHTML.slice(-1) == "+"){
+        return;
+    }else if(result.innerHTML.slice(-1) == "*"){
+        return;
+    }else if(result.innerHTML.slice(-1) == "/"){
+        return;
+    }else if(result.innerHTML.slice(-1) == "."){
+        return;
+    }else{
+        result.innerHTML += input;
+    }
+}
